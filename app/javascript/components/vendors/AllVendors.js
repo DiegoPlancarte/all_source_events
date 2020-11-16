4a0dbd986341a4ba80c290d73b8947ffaf979626
Diego Plancarte <dplancarte7@gmail.com>
2020-11-16 14:40:53 -0800

    Added CRUD hooks. Added pictures for vendors. Completed all vendors page.

--- app/javascript/components/vendors/AllVendors.js
+++ app/javascript/components/vendors/AllVendors.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Button, Spinner } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import useRead from '../hooks/useRead'
+import { Container, Row, Col, Card, Button, Spinner } from 'react-bootstrap';
 
 const AllVendors = () => {
 
   const [ vendors, setVendors, vendorsLoading, vendorErrors ] = useRead('vendors')
+
+  const baker = require('../../../assets/images/baker')
+  const caterer = require('../../../assets/images/caterer')
+  const florist = require('../../../assets/images/florist')
+  const venue = require('../../../assets/images/venue')
 
   if (vendorsLoading) {
     return <div><Spinner animation="border" variant="primary" />Loading...</div>
@@ -13,14 +18,39 @@ const AllVendors = () => {
 
   return (
     <React.Fragment>
-      { vendors.map((v,i) => {
-        return (
-          <div key={i}>
-            <Link to={`/iteminfo/${v.id}`}>{v.name}</Link>
-            <p>{v.user_id}</p>
-          </div>
-        )
-      })}
+        <Container>
+          <h1 className="text-primary" id="header">All Vendors</h1>
+          <hr/>
+          <Row md="1" lg="2" xl="3">
+            {vendors.map((v,i)=> {
+                let imageName = () => {
+                  if (v.category === 'Baker') {
+                    return `${baker}`
+                  } else if (v.category === 'Caterer') {
+                    return `${caterer}`
+                  } else if (v.category === 'Florist') {
+                    return `${florist}`
+                  } else if (v.category === 'Venue') {
+                    return `${venue}`
+                  }
+                }
+                return (
+                  <Col key={v.id} className="vendor-cards">
+                    <Card border="light" className="shadow">
+                      <Card.Img className="img-fluid" src={imageName()}/>
+                      <Card.Body>
+                        <Card.Title><strong>{v.name}</strong></Card.Title>
+                        <Card.Text>{v.category}</Card.Text>
+                        <Link to={`/vendorinfo/${v.id}`} >
+                          <Button className="text-white">Details</Button>
+                        </Link>
+                      </Card.Body>
+                      <Card.Footer>{v.city}, {v.state} {v.zip}</Card.Footer>
+                    </Card>
+                  </Col>
+            )})}
+          </Row>
+        </Container>
     </React.Fragment>
   );
 }
